6b47f5219cf47f287dbac259c9f05f762b406fe5
fDero <francescodero@outlook.it>
2026-05-05 10:31:25 +0200

    Code cleaning of the DoMove logic

--- lib/events/domove.ex
+++ lib/events/domove.ex
@@ -14,29 +14,64 @@ defmodule DoMove do
         :promotion
     ]
 
-    def update_state(game, req, _sender),
-        do: update_state(game, req)
-
     def update_state(game, req) do
-        Game.update_state(game, fn game ->
+        case preliminary_checks(game, req) do
+            {:error, reason} -> {:error, reason}
+            {:ok, :noerrors} -> {:ok, game
+                |> apply_move(req)
+                |> update_history()
+                |> update_pending()
+                |> update_timers(req)
+                |> update_ending()
+            }
+        end
+    end
+
+    defp preliminary_checks(game, req) do
             both_players = [game.players.white, game.players.black]
             fullmove_count = game.board.counters.fullmoves
             player_color = Players.player_color(game.players, req.user)
             piece_color = Squares.color(game.board.squares, req.from)
             cond do
+                game.ending.winner != nil       -> { :error, "game finished"}
+                Clock.game_timed_out?(game)     -> { :error, "game timed out"}
                 req.user not in both_players    -> { :error, "forbidden: not a player" }
                 player_color != game.board.turn -> { :error, "forbidden: not your turn" }
                 piece_color != player_color     -> { :error, "forbidden: not your piece" }
                 req.count != fullmove_count     -> { :error, "corrupted: wrong move count" }
-                true                            -> { :ok, update_ending(game, req) }
+                true                            -> { :ok, :noerrors }
             end
-        end)
     end
 
-    defp update_ending(game, req) do
+    defp apply_move(game, req) do
         new_board = Board.apply_move!(game.board, req.from, req.to, req.promotion)
-        { new_history, _fen } = History.register_move(game.history, game.board)
-        patch = %{ board: new_board, pending: @nopending, history: new_history }
-        Map.merge(game, patch)
+        %{game | board: new_board, pending: @nopending}
+    end
+
+    defp update_history(game) do
+        {new_history, _fen} = History.register_move(game.history, game.board)
+        %{game | history: new_history}
+    end
+
+    defp update_pending(game) do
+        %{game | pending: @nopending}
+    end
+
+    defp update_timers(game, req) do
+        player_color = Players.player_color(game.players, req.user)
+        %{ game | clock: Clock.update_clock(game, player_color) }
+    end
+
+    defp update_ending(game) do
+        {color, opponent} = {game.board.turn, Squares.opponent_color(game.board.turn)}
+        winning = %{winner: opponent, reason: :timeout}
+        losing  = %{winner: color, reason: :checkmate}
+        new_ending = cond do
+            Clock.player_timed_out?(game, color) -> winning
+            Squares.king_status(game.board, color) == :checkmate -> losing
+            Squares.king_status(game.board, color) == :stalemate -> @stalemate
+            true -> @noending
+        end
+        %{game | ending: new_ending}
     end
 end
