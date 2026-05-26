158a356635ced7aacc5cc335b7a255e88168b931
fDero <francescodero@outlook.it>
2026-05-26 22:56:17 +0200

    Minor changes

--- lib/commons/startup.ex
+++ lib/commons/startup.ex
@@ -11,49 +11,47 @@ defmodule Startup do
         Supervisor.start_link(children(), strategy: :one_for_one)
     end
 
-    defp children do
-        [
-            {Cluster.Supervisor, [[
-                main_cluster: [
-                    strategy: @cluster_strategy,
-                    config: @cluster_config
-                ]],
-                [name: :cluster_nodes_supervisor]
-            ]},
-            {Horde.Registry, [
-                name: :matchmaking_registry,
-                keys: :unique,
-                members: :auto
-            ]},
-            {Horde.Registry, [
-                name: :gameplay_registry,
-                keys: :unique,
-                members: :auto
-            ]},
-            {Horde.DynamicSupervisor, [
-                name: :cluster_processes_supervisor,
-                strategy: :one_for_one,
-                members: :auto
-            ]},
-            {Bandit, [
-                port: String.to_integer(System.get_env("port", "4000")),
-                plug: Router,
-                ip: {0, 0, 0, 0}
-            ]},
-            {Mongo, [
-                url: System.get_env("MONGODB_URL", "mongodb://localhost:27017/games"),
-                name: @mongodb_instance
-            ]},
-            {Broker, [
-                dispatcher_concurrency: 10,
-                consumer_concurrency: 1,
-                connection: [
-                    host: System.get_env("RABBITMQ_HOST", "localhost"),
-                    port: String.to_integer(System.get_env("RABBITMQ_PORT", "5672")),
-                    username: System.get_env("RABBITMQ_USER", "guest"),
-                    password: System.get_env("RABBITMQ_PASS", "guest")
-                ]
-            ]}
-        ]
-    end
+    defp children(), do: [
+        {Cluster.Supervisor, [[
+            main_cluster: [
+                strategy: @cluster_strategy,
+                config: @cluster_config
+            ]],
+            [name: :cluster_nodes_supervisor]
+        ]},
+        {Horde.Registry, [
+            name: :matchmaking_registry,
+            keys: :unique,
+            members: :auto
+        ]},
+        {Horde.Registry, [
+            name: :gameplay_registry,
+            keys: :unique,
+            members: :auto
+        ]},
+        {Horde.DynamicSupervisor, [
+            name: :cluster_processes_supervisor,
+            strategy: :one_for_one,
+            members: :auto
+        ]},
+        {Bandit, [
+            port: String.to_integer(System.get_env("port", "4000")),
+            plug: Router,
+            ip: {0, 0, 0, 0}
+        ]},
+        {Mongo, [
+            url: System.get_env("MONGODB_URL", "mongodb://localhost:27017/games"),
+            name: @mongodb_instance
+        ]},
+        {Broker, [
+            dispatcher_concurrency: 10,
+            consumer_concurrency: 1,
+            connection: [
+                host: System.get_env("RABBITMQ_HOST", "localhost"),
+                port: String.to_integer(System.get_env("RABBITMQ_PORT", "5672")),
+                username: System.get_env("RABBITMQ_USER", "guest"),
+                password: System.get_env("RABBITMQ_PASS", "guest")
+            ]
+        ]}
+    ]
 end
